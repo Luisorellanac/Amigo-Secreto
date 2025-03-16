10b914764a9e9f7c44aab56207ceec643dce87a7
Luisorellanac <luiis.orellana@gmail.com>
2025-03-16 13:23:18 -0300

    actualizada app.js

--- Amigo-Secreto/app.js
+++ Amigo-Secreto/app.js
@@ -1,1 +1,7 @@
+let nombreSorteado = 0;
+let intentos = 0;
+let listanombreSorteado = [];
+let numeroMaximo = 5;
+
+
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
